51959bd1b04963e35ab5031438144fc3be4da93f
Ivan <kimagure44@yahoo.es>
2019-02-21 02:36:28 +0100

    Añadido los assets a la tarea de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,12 @@ const babel = require('gulp-babel');
 gulp.task('default', () => {
     gulp.watch('js/main.js', done => {
         gulp.src('js/main.js')
-        .pipe(babel({"presets": ["env"]}))    
-        .pipe(gulp.dest('dist'));
+            .pipe(babel({"presets": ["env"]}))    
+            .pipe(gulp.dest('dist'));
+        gulp.src('assets/*.*')
+            .pipe(gulp.dest('dist/assets'))
+        gulp.src('audio/*.*')
+            .pipe(gulp.dest('dist/audio'))
         done();
     })
 });
